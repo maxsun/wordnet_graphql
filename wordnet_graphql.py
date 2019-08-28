1c2506a25c32c7cfff92e94db10f1ab57e4ec709
Max Sun <max@Maxs-MacBook-Pro.local>
2019-08-28 13:07:54 -0700

    Made linting changes

--- wordnet_graphql.py
+++ wordnet_graphql.py
@@ -16,107 +16,153 @@ def get_lemma_str(lemma: Lemma) -> str:
 
 class WordNetObjectNode(graphene.ObjectType):
 
-    def resolve_type(self, wordnet_object):
-        if isinstance(wordnet_object, Synset):
+    hypernyms = graphene.List(lambda: WordNetObjectNode)
+    instance_hypernyms = graphene.List(lambda: WordNetObjectNode)
+    hyponyms = graphene.List(lambda: WordNetObjectNode)
+    instance_hyponyms = graphene.List(lambda: WordNetObjectNode)
+    member_holonyms = graphene.List(lambda: WordNetObjectNode)
+    substance_holonyms = graphene.List(lambda: WordNetObjectNode)
+    part_holonyms = graphene.List(lambda: WordNetObjectNode)
+    member_meronyms = graphene.List(lambda: WordNetObjectNode)
+    substance_meronyms = graphene.List(lambda: WordNetObjectNode)
+    part_meronyms = graphene.List(lambda: WordNetObjectNode)
+    topic_domains = graphene.List(lambda: WordNetObjectNode)
+    in_topic_domains = graphene.List(lambda: WordNetObjectNode)
+    region_domains = graphene.List(lambda: WordNetObjectNode)
+    in_region_domains = graphene.List(lambda: WordNetObjectNode)
+    usage_domains = graphene.List(lambda: WordNetObjectNode)
+    in_usage_domains = graphene.List(lambda: WordNetObjectNode)
+    attributes = graphene.List(lambda: WordNetObjectNode)
+    entailments = graphene.List(lambda: WordNetObjectNode)
+    causes = graphene.List(lambda: WordNetObjectNode)
+    also_sees = graphene.List(lambda: WordNetObjectNode)
+    verb_groups = graphene.List(lambda: WordNetObjectNode)
+    similar_tos = graphene.List(lambda: WordNetObjectNode)
+
+    def resolve_type(self):
+        if isinstance(self.wordnet_obj, Synset):
             return SynsetNode
-        elif isinstance(wordnet_object, Lemma):
+        elif isinstance(self.wordnet_obj, Lemma):
             return LemmaNode
         else:
             return NotImplementedError
 
     def resolve_hypernyms(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.hypernyms()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.hypernyms()))
 
     def resolve_instance_hypernyms(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.instance_hypernyms()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.instance_hypernyms()))
 
     def resolve_hyponyms(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.hyponyms()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.hyponyms()))
 
     def resolve_instance_hyponyms(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.instance_hyponyms()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.instance_hyponyms()))
 
     def resolve_member_holonyms(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.member_holonyms()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.member_holonyms()))
 
     def resolve_substance_holonyms(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.substance_holonyms()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.substance_holonyms()))
 
     def resolve_part_holonyms(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.part_holonyms()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.part_holonyms()))
 
     def resolve_part_meronyms(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.part_meronyms()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.part_meronyms()))
 
     def resolve_member_meronyms(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.member_meronyms()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.member_meronyms()))
 
     def resolve_substance_meronyms(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.substance_meronyms()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.substance_meronyms()))
 
     def resolve_topic_domains(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.topic_domains()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.topic_domains()))
 
     def resolve_in_topic_domains(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.in_topic_domains()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.in_topic_domains()))
 
     def resolve_region_domains(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.region_domains()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.region_domains()))
 
     def resolve_in_region_domains(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.in_region_domains()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.in_region_domains()))
 
     def resolve_usage_domains(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.usage_domains()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.usage_domains()))
 
     def resolve_in_usage_domains(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.in_usage_domains()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.in_usage_domains()))
 
     def resolve_attributes(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.attributes()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.attributes()))
 
     def resolve_entailments(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.entailments()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.entailments()))
 
     def resolve_causes(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.causes()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.causes()))
 
     def resolve_also_sees(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.also_sees()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.also_sees()))
 
     def resolve_verb_groups(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.verb_groups()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.verb_groups()))
 
     def resolve_similar_tos(self, info):
-        typ = self.resolve_type(wordnet_object=self.wordnet_object)
-        return list(map(lambda x: typ(x), self.wordnet_object.similar_tos()))
+        return list(map(
+            self.resolve_type(),
+            self.wordnet_obj.similar_tos()))
 
-    def __init__(self, wordnet_object):
-        self.wordnet_object = wordnet_object
+    def __init__(self, wordnet_obj):
+        self.wordnet_obj = wordnet_obj
 
 
 class SynsetNode(WordNetObjectNode):
+
     hypernyms = graphene.List(lambda: SynsetNode)
     instance_hypernyms = graphene.List(lambda: SynsetNode)
     hyponyms = graphene.List(lambda: SynsetNode)
@@ -143,17 +189,17 @@ class SynsetNode(WordNetObjectNode):
     pos = graphene.String()
     offset = graphene.Int()
     name = graphene.String()
-    # frame ids
+    # Missing: frame ids
     definition = graphene.String()
-    # examples = graphene.String()
+    # Missing: examples = graphene.String()
     lexname = graphene.String()
-    # lemma_names = graphene.List(graphene.String()
+    # Missing: lemma_names = graphene.List(graphene.String()
     lemmas = graphene.List(lambda: LemmaNode)
     root_hypernyms = graphene.List(lambda: SynsetNode)
     max_depth = graphene.Int()
     min_depth = graphene.Int()
-    # closure
-    # hypernym paths
+    # Missing: closure
+    # Missing: hypernym paths
     common_hypernyms = graphene.List(
         lambda: SynsetNode,
         otherSynsetName=graphene.String())
@@ -181,61 +227,83 @@ class SynsetNode(WordNetObjectNode):
     # Missing: lin_similarity
 
     def resolve_pos(self, info):
-        return self.wordnet_object.pos()
+        return self.wordnet_obj.pos()
 
     def resolve_offset(self, info):
-        return self.wordnet_object.offset()
+        return self.wordnet_obj.offset()
 
     def resolve_name(self, info):
-        return self.wordnet_object.name()
+        return self.wordnet_obj.name()
 
     def resolve_definition(self, info):
-        return self.wordnet_object.definition()
+        return self.wordnet_obj.definition()
 
     def resolve_lexname(self, info):
-        return self.wordnet_object.lexname()
+        return self.wordnet_obj.lexname()
 
     def resolve_lemmas(self, info):
-        return list(map(lambda x: LemmaNode(x), self.wordnet_object.lemmas()))
+        return list(map(lambda x: LemmaNode(x), self.wordnet_obj.lemmas()))
 
     def resolve_root_hypernyms(self, info):
-        return list(map(lambda x: SynsetNode(x), self.wordnet_object.root_hypernyms()))
+        return list(map(
+            lambda x: SynsetNode(x),
+            self.wordnet_obj.root_hypernyms()))
 
     def resolve_max_depth(self, info):
-        return self.wordnet_object.max_depth()
+        return self.wordnet_obj.max_depth()
 
     def resolve_min_depth(self, info):
-        return self.wordnet_object.min_depth()
+        return self.wordnet_obj.min_depth()
 
     def resolve_common_hypernyms(self, info, otherSynsetName):
         otherSynset = wn.synset(otherSynsetName)
-        return list(map(lambda x: SynsetNode(x), self.wordnet_object.common_hypernyms(otherSynset)))
+        return list(map(
+            lambda x: SynsetNode(x),
+            self.wordnet_obj.common_hypernyms(otherSynset)))
 
     def resolve_lowest_common_hypernyms(self, info, otherSynsetName):
         otherSynset = wn.synset(otherSynsetName)
-        return list(map(lambda x: SynsetNode(x), self.wordnet_object.lowest_common_hypernyms(otherSynset)))
+        return list(map(
+            lambda x: SynsetNode(x),
+            self.wordnet_obj.lowest_common_hypernyms(otherSynset)))
 
-    def resolve_shortest_path_distance(self, info, otherSynsetName, simulateRoot=False):
+    def resolve_shortest_path_distance(self, info,
+                                       otherSynsetName, simulateRoot=False):
         otherSynset = wn.synset(otherSynsetName)
-        return self.wordnet_object.shortest_path_distance(otherSynset, simulateRoot)
+        return self.wordnet_obj.shortest_path_distance(
+            otherSynset,
+            simulateRoot)
 
-    def resolve_path_similarity(self, info, otherSynsetName='entity.n.01', simulateRoot=True):
+    def resolve_path_similarity(
+            self,
+            info,
+            otherSynsetName='entity.n.01',
+            simulateRoot=True):
         otherSynset = wn.synset(otherSynsetName)
-        return self.wordnet_object.path_similarity(otherSynset, simulateRoot)
+        return self.wordnet_obj.path_similarity(otherSynset, simulateRoot)
 
-    def resolve_lch_similarity(self, info, otherSynsetName, simulateRoot=True):
+    def resolve_lch_similarity(
+            self,
+            info,
+            otherSynsetName,
+            simulateRoot=True):
         otherSynset = wn.synset(otherSynsetName)
-        return self.wordnet_object.lch_similarity(otherSynset, simulate_root=simulateRoot)
+        return self.wordnet_obj.lch_similarity(otherSynset, simulateRoot)
 
-    def resolve_wup_similarity(self, info, otherSynsetName='entity.n.01', simulateRoot=True):
+    def resolve_wup_similarity(
+            self,
+            info,
+            otherSynsetName='entity.n.01',
+            simulateRoot=True):
         otherSynset = wn.synset(otherSynsetName)
-        return self.wordnet_object.wup_similarity(otherSynset, simulate_root=simulateRoot)
+        return self.wordnet_obj.wup_similarity(otherSynset, simulateRoot)
 
-    def __init__(self, wordnet_object):
-        WordNetObjectNode.__init__(self, wordnet_object)
+    def __init__(self, wordnet_obj):
+        WordNetObjectNode.__init__(self, wordnet_obj)
 
 
 class LemmaNode(WordNetObjectNode):
+
     hypernyms = graphene.List(lambda: LemmaNode)
     instance_hypernyms = graphene.List(lambda: LemmaNode)
     hyponyms = graphene.List(lambda: LemmaNode)
@@ -272,41 +340,55 @@ class LemmaNode(WordNetObjectNode):
     pertainyms = graphene.List(lambda: LemmaNode)
 
     def resolve_name(self, info):
-        return self.wordnet_object.name()
+        return self.wordnet_obj.name()
 
     def resolve_syntactic_marker(self, info):
-        return self.wordnet_object.syntactic_marker()
+        return self.wordnet_obj.syntactic_marker()
 
     def resolve_synset(self, info):
-        return SynsetNode(self.wordnet_object.synset())
+        return SynsetNode(self.wordnet_obj.synset())
 
     def resolve_lang(self, info):
-        return self.wordnet_object.lang()
+        return self.wordnet_obj.lang()
 
     def resolve_key(self, info):
-        return self.wordnet_object.key()
+        return self.wordnet_obj.key()
 
     def resolve_count(self, info):
-        return self.wordnet_object.count()
+        return self.wordnet_obj.count()
 
     def resolve_antonyms(self, info):
-        return list(map(lambda x: LemmaNode(x), self.wordnet_object.antonyms()))
+        return list(map(
+            lambda x: LemmaNode(x),
+            self.wordnet_obj.antonyms()))
 
     def resolve_derivationally_related_forms(self, info):
-        return list(map(lambda x: LemmaNode(x), self.wordnet_object.derivationally_related_forms()))
+        return list(map(
+            lambda x: LemmaNode(x),
+            self.wordnet_obj.derivationally_related_forms()))
 
     def resolve_pertainyms(self, info):
-        return list(map(lambda x: LemmaNode(x), self.wordnet_object.pertainyms()))
+        return list(map(
+            lambda x: LemmaNode(x),
+            self.wordnet_obj.pertainyms()))
 
-    def __init__(self, wordnet_object: _WordNetObject):
-        WordNetObjectNode.__init__(self, wordnet_object)
+    def __init__(self, wordnet_obj: _WordNetObject):
+        WordNetObjectNode.__init__(self, wordnet_obj)
 
 
 class Query(graphene.ObjectType):
 
-    all_synsets = graphene.List(SynsetNode, pos=graphene.String(required=False))
-    synset = graphene.Field(SynsetNode, name=graphene.String())
-    lemma = graphene.Field(LemmaNode, id=graphene.String())
+    all_synsets = graphene.List(
+        SynsetNode,
+        pos=graphene.String(required=False))
+
+    synset = graphene.Field(
+        SynsetNode,
+        name=graphene.String())
+
+    lemma = graphene.Field(
+        LemmaNode,
+        id=graphene.String())
 
     def resolve_all_synsets(self, info, pos=None):
         return list(map(lambda x: SynsetNode(x), wn.all_synsets(pos=pos)))
@@ -314,7 +396,7 @@ class Query(graphene.ObjectType):
     def resolve_synset(self, info, name="entity.n.01"):
         return SynsetNode(wn.synset(name))
 
-    def resolve_lemma(self, info, id):
+    def resolve_lemma(self, info, id="entity.n.01.entity"):
         return LemmaNode(wn.lemma(id))
 
 
